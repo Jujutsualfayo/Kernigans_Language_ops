9387108cf2e998ddb02da15649bc7946257c8261
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2026-05-16 10:11:07 +0000

    test

--- pun.c
+++ pun.c
@@ -7,11 +7,12 @@ int main(void){
     scanf("%d/%d", &numb1, &denom1);
     printf("Input second fraction: ");
     scanf("%d/%d", &numb2, &denom2);
-    
+
     result_num = numb1 * denom2 + numb2 * denom1;
     result_denom = denom1 * denom2;
-    printf("Sum of the fraction is %d/%d\n", result_num, result_denom);
+    printf("The answer is: %d/%d", result_num, result_denom);
 
     return 0;
 }
+
     
